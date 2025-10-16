f4d88cd97a01ee7257b354a7e13598b600d50ab2
Dmitry Salashchenko <ho4houkot39@gmail.com>
2025-10-16 15:13:07 +0300

    feat: 9 tasks complete

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -117,8 +117,11 @@ function removeLeadingWhitespaces(value) {
  *   removeTrailingWhitespaces('cat ') => 'cat'
  *   removeTrailingWhitespaces('\t\t\tHello, World! ') => '\t\t\tHello, World!'
  */
-function removeTrailingWhitespaces(/* value */) {
-  throw new Error('Not implemented');
+function removeTrailingWhitespaces(value) {
+  if (typeof value === 'string') {
+    return value.trimEnd();
+  }
+  return '';
 }
 
 /**
@@ -134,8 +137,11 @@ function removeTrailingWhitespaces(/* value */) {
  *   repeatString('', 3) => ''
  *   repeatString('abc', -2) => ''
  */
-function repeatString(/* str, times */) {
-  throw new Error('Not implemented');
+function repeatString(str, times) {
+  if (typeof times !== 'number' || times < 0 || times % 1 !== 0) {
+    return '';
+  }
+  return str.repeat(times);
 }
 
 /**
@@ -150,8 +156,12 @@ function repeatString(/* str, times */) {
  *   removeFirstOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeFirstOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeFirstOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeFirstOccurrences(str, value) {
+  const index = str.indexOf(value);
+  if (index === -1) {
+    return str;
+  }
+  return str.slice(0, index) + str.slice(index + value.length);
 }
 
 /**
